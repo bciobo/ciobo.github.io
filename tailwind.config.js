c38efd1355a2161660bcdd2f10f9392553a603ac
bciobo <b.ciobotaru@solvemate.com>
2020-07-06 19:04:26 +0200

    * replace SCSS preprocessor with PostCSS following the Tailwind docs: is a PostCSS plugin; little need for custom css > preprocessor slows down compilation * add main styles file and use Tailwind classes on a button

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,6 @@
 module.exports = {
-  purge: [],
-  theme: {
-    extend: {},
-  },
-  variants: {},
-  plugins: [],
+    purge: ["./src/**/*.js", "./src/**/*.jsx", "./src/**/*.ts", "./src/**/*.tsx"],
+    theme: {},
+    variants: {},
+    plugins: [],
 }
